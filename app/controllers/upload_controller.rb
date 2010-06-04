4da2fa1ad1ed5d63de35f354a97531127e7b20a5
Sharon <s.sll@sbcglobal.net>
2010-06-04 10:21:39 -0700

    before filter for newsletters

--- app/controllers/upload_controller.rb
+++ app/controllers/upload_controller.rb
@@ -1,29 +1,46 @@
 class UploadController < ApplicationController
   
+  before_filter :verify
+  
+  
   #Agile Web Development with Rails
   def get
-    @newsletter = Newsletter.new
+    if verify
+      @newsletter = Newsletter.new
+    else
+      redirect_to (:controller => 'newsletters', :action => 'index')
+    end
   end
   
   def save
-    @newsletter = Newsletter.new(params[:newsletter])
-    if @newsletter.save
-       flash[:notice] = "Newsletter added for " + @newsletter.str_month + " " + @newsletter.year.to_s()
-      redirect_to (:action => 'get')
+    if verify
+      @newsletter = Newsletter.new(params[:newsletter])
+      if @newsletter.save
+        flash[:notice] = "Newsletter added for " + @newsletter.str_month + " " + @newsletter.year.to_s()
+        redirect_to (:action => 'get')
+      else
+        render(:action => :get)
+      end
     else
-      render(:action => :get)
+      flash[:notice] = "login needed"
+      redirect_to (:controller => 'newsletters', :action => 'index')
     end
   end
   
   def newsletter
     @newsletter = Newsletter.find(params[:id])
     send_data(@newsletter.data, :filename => @newsletter.name,
-#:type => @newsletter.content_type,
+    #:type => @newsletter.content_type,
 :disposition => "inline" )
   end
   
   def show
     @newsletter = Newsletter.find(params[:id])
+  end
+  
+  private
+  def verify
+    session[:admin]
   end
   
 end
